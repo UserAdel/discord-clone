a85be7b8bd6212e2dc1282214e20d3c650aebc69
adel hany <adel58420@gmail.com>
2025-11-12 22:46:29 +0200

    Direct Messages

---
+++ pages/api/socket/direct-messages/[directMessageId].ts
@@ -0,0 +1,141 @@
+import { NextApiRequest } from "next";
+import { NextApiResponseServerIo } from "@/types";
+import { currentProfilePages } from "@/lib/current-profile-pages";
+import { db } from "@/lib/db";
+import { MemberRole } from "@/lib/generated/prisma";
+import { error } from "console";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponseServerIo
+) {
+  if (req.method !== "DELETE" && req.method !== "PATCH") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+    const profile = await currentProfilePages(req);
+    const { directMessageId, conversationId } = req.query;
+    const { content } = req.body;
+    if (!profile) {
+      return res.status(401).json({ error: "Unathorized" });
+    }
+    if (!conversationId) {
+      return res.status(401).json({ error: "conversationId Id Missing" });
+    }
+
+    // if (!messageId) {
+    //   return res.status(401).json({ error: "Message Id Missing" });
+    // }
+
+    const conversation = await db.conversation.findFirst({
+      where: {
+        id: conversationId as string,
+        OR: [
+          {
+            memberOne: {
+              profileId: profile.id,
+            },
+          },
+          {
+            memberTwo: {
+              profileId: profile.id,
+            },
+          },
+        ],
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+
+    if (!conversation) {
+      return res.status(404).json({ error: "conversation Not Found " });
+    }
+    const member =
+      conversation.memberOne.profileId === profile.id
+        ? conversation.memberOne
+        : conversation.memberTwo;
+
+    if (!member) {
+      return res.status(404).json({ error: "Member Not Found " });
+    }
+    let directMessage = await db.directMessage.findFirst({
+      where: {
+        id: directMessageId as string,
+        conversationId: conversationId as string,
+      },
+      include: {
+        Member: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+    if (!directMessage || directMessage.deleted) {
+      return res.status(404).json({ error: "directMessage Not Found " });
+    }
+    const isMessageOwner = directMessage.memberId === member.id;
+    const isAdmin = member.role === MemberRole.ADMIN;
+    const isModerator = member.role === MemberRole.MODERATOR;
+    const canModify = isMessageOwner || isAdmin || isModerator;
+    if (!canModify) {
+      return res.status(401).json({ error: "Unathorized" });
+    }
+    if (req.method === "DELETE") {
+      directMessage = await db.directMessage.update({
+        where: {
+          id: directMessageId as string,
+        },
+        data: {
+          fileUrl: null,
+          content: "This message has been deleted",
+          deleted: true,
+        },
+        include: {
+          Member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+      });
+    }
+    if (req.method === "PATCH") {
+      if (!isMessageOwner) {
+        return res.status(401).json({ error: "Unathorized" });
+      }
+      directMessage = await db.directMessage.update({
+        where: {
+          id: directMessageId as string,
+        },
+        data: {
+          content,
+        },
+        include: {
+          Member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+      });
+    }
+    const updateKey = `chat:${conversation.id}:${directMessageId}`;
+    res.socket.server.io.emit(updateKey, directMessage);
+    return res.status(200).json(directMessage);
+  } catch (error) {
+    console.log("[MESSAGE_ID]", error);
+    return res.status(500).json({ error: "Internal Error" });
+  }
+}
